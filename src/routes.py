7fc384122ffba69fadf22627f3796827aa982ada
sinavarasina <varasinafarmadani@gmail.com>
2025-12-11 18:48:03 +0700

    add endpoint type validation

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,12 @@
 def includeme(config):
     config.add_route('users_list',   '/users')
     config.add_route('users_login',  '/users/login')
-    config.add_route('users_detail', '/users/{id}')
-    config.add_route('users_events', '/users/{id}/events')
-    config.add_route('users_bookings', '/users/{id}/bookings')
+    config.add_route('users_detail', '/users/{id:\d+}')
+    config.add_route('users_events', '/users/{id:\d+}/events')
+    config.add_route('users_bookings', '/users/{id:\d+}/bookings')
 
     config.add_route('events_list',   '/events')
-    config.add_route('events_detail', '/events/{id}')
+    config.add_route('events_detail', '/events/{id:\d+}')
 
     config.add_route('bookings_create', '/bookings')
     config.add_route('bookings_detail', '/bookings/{code}')
